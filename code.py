66c4c308224f7db6149f876b5ec2080230dfe662
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2022-09-12 09:49:57 -0400

    rgb color change code

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 import board
 import neopixel
+import time
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 dot.brightness = 0.5 
@@ -8,3 +9,9 @@ print("Make it red!")
 
 while True:
     dot.fill((255, 0, 0))
+    time.sleep(1)
+    dot.fill((0, 102, 0))
+    time.sleep((1))
+    dot.fill((0, 0, 255))
+    time.sleep((1))
+
