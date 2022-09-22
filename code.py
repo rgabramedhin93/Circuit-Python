cc582a49c490abd0547f12a51290512c02f58c19
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2022-09-22 09:48:13 -0400

    added simpleio and code

--- code.py
+++ code.py
@@ -1,14 +1,28 @@
 import time
 import board
 import adafruit_hcsr04
+import neopixel
 
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D7, echo_pin=board.D1)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.5
+
 while True:
     try:
-        print((sonar.distance,))
+        distance = sonar.distance 
+        print((distance))
     except RuntimeError:
         print("Retrying!")
-    time.sleep(0.1)
+    time.sleep(0.1) 
+    if sonar.distance > 20:
+        dot.fill((0, 255, 0))
+    else:
+        dot.fill((0, 0, 255))
+    if sonar.distance < 20 and sonar.distance > 5:
+        dot.fill((0, 0, 255))
+        
+
+    
 
 
 
