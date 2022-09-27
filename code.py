b5d51d165ee5bb13be8a96cd58816ad4e8262b10
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2022-09-27 09:33:16 -0400

    finished rgb code

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import board
 import adafruit_hcsr04
 import neopixel
 import simpleio 
+distance = 0
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -13,16 +14,17 @@ while True:
         distance = sonar.distance 
         print((distance))
     except RuntimeError:
-        print("Retrying!")
+        print("oopsy daisy!")
     time.sleep(0.1) 
-    if sonar.distance >= 5 and sonar.distance <= 20:
-        simpleio.map_range(sonar.distance,5,20,0,255)
+    if distance >= 5 and distance <= 20:
+        simpleio.map_range(distance,5,20,0,255)
         dot.fill((0, 0, 255))
     else:
-        dot.fill((0, 0, 255))
-    if sonar.distance >= 0 and sonar.distance <= 5:
-         simpleio.map_range(sonar.distance,5,20,0,255)
-         dot.fill((0, 0, 255))
+        dot.fill((255, 0, 0))
+    if distance >= 0 and distance <= 5:
+         simpleio.map_range(distance,5,0,255,0)
+         dot.fill((0, 255, 0))
+
         
 
     
