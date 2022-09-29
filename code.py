7850e5c51aeaecc2ec63f6b2a790e2584529b00a
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2022-09-29 09:41:31 -0400

    added new files

--- code.py
+++ code.py
@@ -1,29 +1,15 @@
-import time
 import board
-import adafruit_hcsr04
-import neopixel
-import simpleio 
-distance = 0
+from lcd.lcd import LCD
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
-dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness = 0.5
+# get and i2c object
+i2c = board.I2C()
 
-while True:
-    try:
-        distance = sonar.distance 
-        print((distance))
-    except RuntimeError:
-        print("oopsy daisy!")
-    time.sleep(0.1) 
-    if distance >= 5 and distance <= 20:
-        simpleio.map_range(distance,5,20,0,255)
-        dot.fill((0, 0, 255))
-    else:
-        dot.fill((255, 0, 0))
-    if distance >= 0 and distance <= 5:
-         simpleio.map_range(distance,5,0,255,0)
-         dot.fill((0, 255, 0))
+# some LCDs are 0x3f... some are 0x27.
+lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
+
+lcd.print("Hello, Engineer!")
+
 
         
 
