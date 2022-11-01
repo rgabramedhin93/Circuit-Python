26e083ba5f729889c0b66cb3e74e08142c53b76c
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2022-11-01 10:42:37 -0400

    motor control starting it code

--- code.py
+++ code.py
@@ -1,60 +1,23 @@
 import board
 import time
-from lcd.lcd import LCD
-from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+import pwmio
+import adafruit_motor.servo 
+from analogio import AnalogIn
 from digitalio import DigitalInOut, Direction, Pull
 
-# get and i2c object
-i2c = board.I2C()
-btn = DigitalInOut(board.D3)
-btn2 = DigitalInOut(board.D2)
-btn.direction = Direction.INPUT
-btn.pull = Pull.UP
-btn2.direction = Direction.INPUT
-btn2.pull = Pull.UP
-# some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
-cur_state = True
-prev_state = True
-cur_state2 = True
-prev_state2 = True
-buttonPress = 0
+pwm = pwmio.PWMOut(board.D3, frequency=50)
+pot = AnalogIn(board.A0)
+
 
 while True:
-    while btn2.value == False:
-        lcd.set_cursor_pos(1,0)
-        lcd.print("UP  ")
-        cur_state = btn.value
-        if cur_state != prev_state:
-            if not cur_state:
-                buttonPress = buttonPress + 1
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-            else:
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-        prev_state = cur_state
-    else:
-        lcd.set_cursor_pos(1,0)
-        lcd.print("DOWN")
-        cur_state2 = btn.value
-        if cur_state2 != prev_state2:
-            if not cur_state2:
-                buttonPress = buttonPress - 1
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-            else:
-                lcd.clear()
-                lcd.set_cursor_pos(0,0)
-                lcd.print(str(buttonPress))
-        prev_state2 = cur_state2
+    ``` 
+    AnalogIn
+     pot = analog_in.value
 
-        
+    angle = simpleio.map_range(pot, 0, 65535, 0, 180)
 
+    my_servo.angle = angle
+
+    time.sleep(0.1)
+    ```
     
-
-
-
