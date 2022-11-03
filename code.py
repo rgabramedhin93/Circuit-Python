20ff0696d8cf703b69923acec7d1f756b0c24a6c
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2022-11-03 09:55:51 -0400

    motor control new code that may work

--- code.py
+++ code.py
@@ -1,23 +1,30 @@
 import board
-import time
 import pwmio
-import adafruit_motor.servo 
+from adafruit_motorkit import servo
 from analogio import AnalogIn
-from digitalio import DigitalInOut, Direction, Pull
 
-pwm = pwmio.PWMOut(board.D3, frequency=50)
-pot = AnalogIn(board.A0)
+# servo pin for the M0 boards:
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+my_servo = servo.Servo(pwm)
+angle = 0
+
+# potentiometer
+trimpot = AnalogIn(board.A0)  # pot pin for servo control
+
+def remap_range(value, left_min, left_max, right_min, right_max):
+    # this remaps a value from original (left) range to new (right) range
+    # Figure out how 'wide' each range is
+    left_span = left_max - left_min
+    right_span = right_max - right_min
+
+    # Convert the left range into a 0-1 range (int)
+    value_scaled = int(value - left_min) / int(left_span)
+
+    # Convert the 0-1 range into a value in the right range.
+    return int(right_min + (value_scaled * right_span))
 
 
 while True:
-    ``` 
-    AnalogIn
-     pot = analog_in.value
-
-    angle = simpleio.map_range(pot, 0, 65535, 0, 180)
-
+    angle = remap_range(trimpot.value, 0, 65535, 0, 180)
     my_servo.angle = angle
-
-    time.sleep(0.1)
-    ```
     
