2796523c190d4f85600f5b8843412c36797e393a
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2023-03-29 13:51:52 -0400

    i think almost finished temp sensor code

--- code_1.py
+++ code_1.py
@@ -1,10 +1,28 @@
 import board
 import time
 import analogio
+import digitalio
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
-from digitalio import DigitalInOut, Direction, Pull
 TMP36_PIN = board.A1  # Analog input connected to TMP36 output.
+i2c = board.I2C()
+lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
+
+# turn on lcd power switch pin
+lcdPower = digitalio.DigitalInOut(board.D8)
+lcdPower.direction = digitalio.Direction.INPUT
+lcdPower.pull = digitalio.Pull.DOWN
+
+# Keep the I2C protocol from running until the LCD has been turned on
+# You need to flip the switch on the breadboard to do this.
+while lcdPower.value is False:
+    print("still sleeping")
+    time.sleep(0.1)
+
+# Time to start up the LCD!
+time.sleep(1)
+print(lcdPower.value)
+print("running")
 
 
 # Function to simplify the math of reading the temperature.
@@ -23,5 +41,14 @@ while True:
     # Convert to Fahrenheit.
     temp_F = (temp_C * 9/5) + 32
     # Print out the value and delay a second before looping again.
-    print("Temperature: {}C {}F".format(temp_C, temp_F))
-    time.sleep(1.0)
+    if temp_C > 9:
+        lcd.print("too hot")
+        print("too hot")
+        lcd.clear()
+        time.sleep(1)
+        
+    else:
+        lcd.print("Brr too cold") 
+        print("Brr too cold") 
+        lcd.clear()
+        time.sleep(1)
