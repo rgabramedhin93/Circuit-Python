748a78f88af30b7c12576b8c421b2fbe129ba408
rgabramedhin93 <rgabram93@charlottesvilleschools.org>
2023-03-29 13:51:34 -0400

    i think almost finished temp sensor code

--- code_1.py
+++ code_1.py
@@ -1,68 +1,27 @@
-# SPDX-FileCopyrightText: 2018 Anne Barela for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
-import time
-from adafruit_motor import stepper 
 import board
-from analogio import AnalogIn
-import digitalio
-import pwmio
-import simpleio
-import lib.smoothing
- 
-
-pot = AnalogIn(board.A0)
-STEPS = 200 
-#limit = digitalio.DigitalInOut(board.D8) 
-#limit.direction = digitalio.Direction.INPUT 
-#limit.pull = digitalio.Pull.UP 
-
-coils = (
-    digitalio.DigitalInOut(board.D12),  # A1  #setup for the stepper
-    digitalio.DigitalInOut(board.D11),  # A2
-    digitalio.DigitalInOut(board.D9),   # B1
-    digitalio.DigitalInOut(board.D10),  # B2
-)
+import time
+import analogio
+from lcd.lcd import LCD
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+from digitalio import DigitalInOut, Direction, Pull
+TMP36_PIN = board.A1  # Analog input connected to TMP36 output.
 
 
-for coil in coils:
-    coil.direction = digitalio.Direction.OUTPUT
-
-smoothing = lib.smoothing.Smoothing()  #defining the library of smoothing.py in this file
-Steps= 0 
-motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
+# Function to simplify the math of reading the temperature.
+def tmp36_temperature_C(analogin):
+    millivolts = analogin.value * (analogin.reference_voltage * 1000 / 65535)
+    return (millivolts - 500) / 10
 
 
+# Create TMP36 analog input.
+tmp36 = analogio.AnalogIn(TMP36_PIN)
+
+# Loop forever.
 while True:
-    # smoothvalue = int(simpleio.map_range(pot.value,0,65535,0,4500))
-     
-    #4500 end point of the range of the stepper
-    smoothvalue = int(simpleio.map_range(smoothing.update(pot.value),0,65535,0,4500))   #Use smoothing to determine where the potentiometer is within our mapped range. 
-    print("smoothvalue", smoothvalue, "   Steps" , Steps) 
-    #print(limit.value) 
-    time.sleep(.01)
-    
-    if abs(smoothvalue - Steps) >4: 
-        #print(smoothvalue, Steps) 
-        
-        if smoothvalue > Steps:      #moving the stepper to the mapped and smoothed value and counting the steps
-           motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-           Steps = Steps +1       
-        time.sleep(.001)  
-
-
-        if smoothvalue < Steps:       #moving the stepper backwards when neccesary. Also going to the smoothed and mapped value
-           motor.onestep(style=stepper.DOUBLE, direction= stepper.FORWARD)
-           Steps = Steps -1          
-        time.sleep(.001) 
-    
-
-
-   
-     
- 
-     
-#while target != steps
-    #if target is > steps
-        #move stepper up one step and add 1 to steps 
+    # Read the temperature in Celsius.
+    temp_C = tmp36_temperature_C(tmp36)
+    # Convert to Fahrenheit.
+    temp_F = (temp_C * 9/5) + 32
+    # Print out the value and delay a second before looping again.
+    print("Temperature: {}C {}F".format(temp_C, temp_F))
+    time.sleep(1.0)
